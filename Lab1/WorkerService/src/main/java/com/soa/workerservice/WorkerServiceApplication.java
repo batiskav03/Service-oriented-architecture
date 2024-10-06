57273d9c282386a98276a9bc428dc072fa1f601f
blondyboy <kalabokmaksim@gmail.com>
2024-10-06 16:14:24 +0300

    add two services with dummy object's

---
+++ Lab1/WorkerService/src/main/java/com/soa/workerservice/WorkerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.soa.workerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.soa.workerservice.WorkerServiceApplication.class, args);
+    }
+
+}
---
+++ Lab1/WorkerService/src/test/java/com/soa/workerservice/WokerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.workerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
