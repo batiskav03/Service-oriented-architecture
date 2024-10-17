31ae0737937d391da7b3cc97503113a4542b2340
Arseny Konovalov <k.arseny03@gmail.com>
2024-10-17 17:44:42 +0300

    added initdb script

---
+++ Lab1/Infrostructure/initdb.sql
@@ -0,0 +1,26 @@
+create table coordinates
+(
+    coordinates_uuid UUID PRIMARY KEY,
+    x bigint,
+    y int
+)
+
+create table users
+(
+    user_uuid UUID PRIMARY KEY,
+    name TEXT,
+    coordinates UUID REFERENCES coordinates (coordinates_uuid),
+    localDate DATE,
+    salary int,
+    status TEXT,
+    person UUID REFERENCES persons (person_uuid),
+);
+
+create table persons
+(
+    person_uuid PRIMARY KEY,
+    birthday DATE,
+    passport_uuid UUID,
+    hairColor TEXT,
+    nationality TEXT
+);
