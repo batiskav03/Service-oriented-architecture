8fdd568b7135a8140b75967c9248a205d26b4a16
Arseny Konovalov <k.arseny03@gmail.com>
2024-10-17 18:15:42 +0300

    fixed compose volumes and sql script

--- Lab1/Infrostructure/scripts/initdb.sql
+++ Lab1/Infrostructure/scripts/initdb.sql
@@ -3,15 +3,24 @@ create table coordinates
     coordinates_uuid UUID PRIMARY KEY,
     x bigint,
     y int
-)
+);
 
 create table persons
 (
-    person_uuid PRIMARY KEY,
+    person_uuid UUID PRIMARY KEY,
     birthday DATE,
     passport_uuid UUID,
     hairColor TEXT,
     nationality TEXT
+);
+
+create table locations
+(
+    location_uuid UUID PRIMARY KEY,
+    x bigint,
+    y float,
+    z float,
+    name TEXT
 );
 
 create table users
@@ -19,10 +28,11 @@ create table users
     user_uuid UUID PRIMARY KEY,
     name TEXT,
     coordinates UUID REFERENCES coordinates (coordinates_uuid),
-    localDate DATE,
+    creationTime DATE,
     salary int,
+    startDate DATE,
     status TEXT,
-    person UUID REFERENCES persons (person_uuid),
+    person UUID REFERENCES persons (person_uuid)
 );
 
 
