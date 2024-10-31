fa335e67cfdbe0b89107263cae3c80b5b5456ca7
batiskav03 <kalabokmaksim@gmail.com>
2024-10-31 17:51:47 +0300

    refactor initdb.sql add jaxrs HRController

--- Lab1/HR-Service/src/main/java/com/soa/hrservice/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.soa.hrservice;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ Lab1/HR-Service/src/main/java/com/soa/hrservice/controller/HRController.java
@@ -0,0 +1,42 @@
+package com.soa.hrservice.controller;
+
+import com.soa.hrservice.service.WorkerRequestService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
+
+
+@Path("/hr")
+public class HRController {
+
+    @Inject
+    private WorkerRequestService workerRequestService;
+
+
+    @DELETE
+    @Path("fire/{id}")
+    public Response fireWorker(@PathParam("id") String id) {
+        workerRequestService.doQuery();
+        System.out.println(id);
+        UUID uuid;
+        try {
+            uuid = UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return Response.status(400)
+                    .entity("Invalid parameters supplied,\n" +
+                            "id is uncorrected, change id and retry x")
+                    .build();
+        }
+//        Worker worker = workerService.getWorker(uuid);
+//        if (worker == null) {
+//            return null;
+//        }
+        return Response.ok().build();
+    }
+
+}
